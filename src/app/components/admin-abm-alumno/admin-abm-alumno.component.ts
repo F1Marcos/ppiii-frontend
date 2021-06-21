29f6b20c30d67ef6f2e46b7da0b16ff8bb0bd078
MatiasEz <matialeezcurra@gmail.com>
2021-06-21 19:47:27 -0300

    Cambios varios

--- src/app/components/admin-abm-alumno/admin-abm-alumno.component.ts
+++ src/app/components/admin-abm-alumno/admin-abm-alumno.component.ts
@@ -19,8 +19,12 @@ export class AdminAbmAlumnoComponent implements OnInit {
     mail:"",
     rol:""
   };
+  errorUsuario=0;
   errorNombre=0;
-  errorPrecio=0;
+  errorApellido=0;
+  errorDNI=0;
+  errorEmail=0;
+  errorRol=0;
   alert:boolean=false;
 
 
@@ -91,4 +95,135 @@ export class AdminAbmAlumnoComponent implements OnInit {
 			  console.log(err.error);}
 		  );
   }
+
+  verificarForm():boolean{
+    this.errorUsuario=this.verificarUsuario(this.use.usuario);
+    this.errorNombre=this.verificarNombre(this.use.nombres);
+    this.errorApellido=this.verificarApellido(this.use.apellidos);
+    this.errorDNI=this.verificarDNI(this.use.dni);
+    this.errorEmail=this.verificarEmail(this.use.mail);
+    this.errorRol=this.verificarRol(this.use.rol);
+    if(  (this.errorNombre+this.errorUsuario+this.errorApellido+this.errorDNI+this.errorEmail+this.errorRol)>0){
+      return false;
+    }
+    return true;
+  }
+
+  verificarUsuario(nombre:string):number {
+    const patron=/^[a-zA-Z]+$/;
+    if(nombre.length==0)
+      return 1;
+    if(nombre.length>20)
+      return 2;
+    if(!patron.test(nombre))
+      return 3;
+    return 0;
+  }
+  verificarNombre(nombre:string):number {
+    const patron=/^\b(?!.*?\s{2})[A-Za-z ]{1,50}\b$/;
+    console.log(nombre);
+    if(nombre.length==0)
+      return 1;
+    if(nombre.length>20)
+      return 2;
+    if(!  patron.test(nombre))
+      return 3;
+    return 0;
+  }
+  verificarApellido(apellido:string):number {
+    const patron=/^\b(?!.*?\s{2})[A-Za-z ]{1,50}\b$/;
+    if(apellido.length==0)
+      return 1;
+    if(apellido.length>20)
+      return 2;
+    if(!patron.test(apellido))
+      return 3;
+    return 0;
+  }
+  verificarDNI(dni:string):number {
+    console.log(dni);
+    const patron=/^[0-9]*$/;
+    if(dni.length==0)
+      return 1;
+    if(dni.length>12)
+      return 2;
+    if(!patron.test(dni))
+      return 3;
+    return 0;
+  }
+  verificarEmail(mail:string):number {
+    const patron=/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    if(mail.length==0)
+      return 1;
+    if(mail.length>20)
+      return 2;
+    if(!patron.test(mail))
+      return 3;
+    return 0;
+  }
+    verificarRol(rol:string):number {
+    const patron=/^[1-2]*$/;
+    if(rol.length==0)
+      return 1;
+    if(rol.length>1)
+      return 2;
+     if(!patron.test(rol))
+      return 2;
+    return 0;
+  }
+
+  limpiarUsuario() {
+    if (this.errorUsuario > 0) {
+      this.use.usuario = "";
+      this.errorUsuario = 0;
+    }
+  }
+  limpiarNombre() {
+    if (this.errorNombre > 0) {
+      this.use.nombres = "";
+      this.errorNombre = 0;
+    }
+  }
+  limpiarApellido() {
+    if (this.errorApellido > 0) {
+      this.use.apellidos = "";
+      this.errorApellido = 0;
+    }
+  }
+  limpiarDNI() {
+    if (this.errorDNI > 0) {
+      this.use.dni = "";
+      this.errorDNI = 0;
+    }
+  }
+  limpiarEmail() {
+    if (this.errorEmail > 0) {
+      this.use.mail = "";
+      this.errorEmail = 0;
+    }
+  }
+  limpiarRol() {
+    if (this.errorRol > 0) {
+      this.use.rol = "";
+      this.errorRol = 0;
+    }
+  }
+
+  limpiarTodo(){
+    this.use={
+      usuario:"", 
+      nombres:"",
+      apellidos:"",
+      dni:"",
+      mail:"",
+      rol:""
+    };
+    this.errorUsuario=0;
+    this.errorNombre=0;
+    this.errorApellido=0;
+    this.errorDNI=0;
+    this.errorEmail=0;
+    this.errorRol=0;
+  }
+
 }
--- src/app/components/admin-abm-materia/admin-abm-materia.component.ts
+++ src/app/components/admin-abm-materia/admin-abm-materia.component.ts
@@ -10,29 +10,30 @@ export class AdminAbmMateriaComponent implements OnInit {
 
   constructor(private usuariosService:UsuariosService) { }
   materias: any = [];
-  mat: any = [];
+  mat={
+    idMat:"", 
+    nombreMat:"",
+    anio:"",
+    tipo:"",
+    flagCorre:0
+  };
 
-  // mat={   // Utilizado para agregar!
-  //   usuario:"", 
-  //   nombres:"",
-  //   apellidos:"",
-  //   dni:"",
-  //   mail:"",
-  //   rol:""
-  // };
-
+  errorID=0;
   errorNombre=0;
-  errorPrecio=0;
+  errorAnio=0;
+  errorTipo=0;
   alert:boolean=false;
 
   ngOnInit(): void {
     this.usuariosService.listarMaterias().subscribe(
       res => {
+        console.log("RES DE SERVICE");
         console.log(res);
         this.materias=res;
         console.log(this.materias);
       },
       err => {
+        console.log("ERR DE SERVICE");
         console.log(err.error.message);    
       }
     );
@@ -54,7 +55,7 @@ export class AdminAbmMateriaComponent implements OnInit {
   eliminarMateria(mat:any){
     console.log('FE: Entre metod post eliminar materia!');
     console.log(mat);
-    if(confirm("Esta seguro que desea eliminar al materia "+mat.nombreMat)) {
+    if(confirm("Esta seguro que desea eliminar al materia "+mat.nombreMat+" esto puede causar problemas de haber registros en la BD")) {
       console.log("Implement delete functionality here");
     
     this.usuariosService.eliminarMateria(mat.idMat).subscribe(
@@ -83,4 +84,93 @@ export class AdminAbmMateriaComponent implements OnInit {
 			  console.log(err.error);}
 		  );
   }
+
+  verificarForm():boolean{
+    this.errorID=this.verificarID(this.mat.idMat);
+    this.errorNombre=this.verificarNombre(this.mat.nombreMat);
+    this.errorAnio=this.verificarAnio(this.mat.anio);
+    this.errorTipo=this.verificarTipo(this.mat.tipo);
+    if(  (this.errorNombre+this.errorID+this.errorAnio+this.errorTipo)>0){
+      return false;
+    }
+    return true;
+  }
+
+  verificarID(id:string):number {
+    const patron=/^[1-3]*$/;
+    if(id.length==0)
+      return 1;
+    if(id.length>6)
+      return 2;
+    if(!patron.test(id))
+      return 3;
+    return 0;
+  }
+  verificarNombre(nombre:string):number {
+    const patron=/^\b(?!.*?\s{2})[A-Za-z ]{1,50}\b$/;
+    console.log(nombre);
+    if(nombre.length==0)
+      return 1;
+    if(nombre.length>20)
+      return 2;
+    if(! patron.test(nombre))
+      return 3;
+    return 0;
+  }
+  verificarAnio(anio:string):number {
+    const patron=/^[1-3]*$/;
+    if(anio.length==0)
+      return 1;
+    if(anio.length>20)
+      return 2;
+    if(!patron.test(anio))
+      return 3;
+    return 0;
+  }
+  verificarTipo(tipo:string):number {
+    if(!(tipo=='Cuatrimestral'|| tipo=='Anual'))
+      return 1;
+    return 0;
+  }
+  limpiarID() {
+    if (this.errorID > 0) {
+      this.mat.idMat = "";
+      this.errorID = 0;
+    }
+  }
+  limpiarNombre() {
+    if (this.errorNombre > 0) {
+      this.mat.nombreMat = "";
+      this.errorNombre = 0;
+    }
+  }
+  limpiarAnio() {
+    if (this.errorAnio > 0) {
+      this.mat.anio = "";
+      this.errorAnio = 0;
+    }
+  }
+  limpiarTipo() {
+    if (this.errorTipo > 0) {
+      this.mat.tipo = "";
+      this.errorTipo = 0;
+    }
+  }
+
+  
+  limpiarTodo(){
+    this.mat={
+      idMat:"", 
+      nombreMat:"",
+      anio:"",
+      tipo:"",
+      flagCorre:0
+    };
+  
+    this.errorID=0;
+    this.errorNombre=0;
+    this.errorAnio=0;
+    this.errorTipo=0;
+  }
 }
+
